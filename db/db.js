3d0f12584f5f19d949668538d8957313d6ef9a40
Ansalpandey <pandeyansal77@gmail.com>
2024-07-03 20:44:33 +0530

    refactor: Update MongoDB connection URL
    The commit updates the MongoDB connection URL in the `db.js` file to use the environment variable `MONGO_URL` instead of `MONGO_URL_LOCAL`. This change ensures that the application connects to the correct MongoDB instance and improves the maintainability of the codebase.

--- db/db.js
+++ db/db.js
@@ -4,8 +4,8 @@ dotenv.config();
 const connectDB = async () => {
   try {
     await mongoose.connect(
-      // process.env.MONGO_URL,
-      process.env.MONGO_URL_LOCAL,
+      process.env.MONGO_URL,
+      // process.env.MONGO_URL_LOCAL,
       {
         useNewUrlParser: true,
         useUnifiedTopology: true,
