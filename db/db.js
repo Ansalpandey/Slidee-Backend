ea94fdb3d757359bccbc83a634019b220bf719df
Ansalpandey <pandeyansal77@gmail.com>
2024-08-02 16:04:09 +0530

    refactor: Remove firebase-admin dependency and update package.json

--- db/db.js
+++ db/db.js
@@ -8,8 +8,8 @@ dotenv.config();
 const connectDB = async () => {
   try {
     await mongoose.connect(
-      // process.env.MONGO_URL,
-      process.env.MONGO_URL_LOCAL,
+      process.env.MONGO_URL,
+      // process.env.MONGO_URL_LOCAL,
       {
         useNewUrlParser: true,
         useUnifiedTopology: true,
